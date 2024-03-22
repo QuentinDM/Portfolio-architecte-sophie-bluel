3f1acbc582b52a253fe5df8960bd74dae7cf2d2b
QuentinDM <88782817+QuentinDM@users.noreply.github.com>
2024-03-22 17:28:36 +0800

    Delete some HTML from div gallery and and scipt.js, comunicate with api works

---
+++ scipt.js
@@ -0,0 +1,43 @@
+  // Function to create DOM elements from working data
+function createWorksElements(data) {
+  // select DOM Element classe .gallery
+  const divGallery = document.querySelector(".gallery");
+  
+  // Loop, Add image and title into figure tag and this figur tag add into Dom element div class .gallery
+  for (let i = 0; i < data.length; i++) {
+    const works = data[i];
+
+    const worksElement = document.createElement("figure");
+
+    const imageElement = document.createElement("img");
+    imageElement.src = works.imageUrl;
+
+    const titleElement = document.createElement("figcaption");
+    titleElement.innerHTML = works.title;
+
+    // Add the figur element to the DOM in the divGallery
+    divGallery.appendChild(worksElement);
+    // Add img element to DOM to display image and title
+    worksElement.appendChild(imageElement);
+    worksElement.appendChild(titleElement);
+  }
+}
+
+// Request Get to URL woks from API 
+fetch("http://localhost:5678/api/works")
+  //When promise is return, response convert HTTP in object JSON
+  .then(response => {
+    //Check if response is responding
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  // receive data from response.JSON() and call createWorksElements(data) function
+  .then(data => {
+    createWorksElements(data);
+  })
+  // Handle Error
+  .catch(error => {
+    console.error('Une erreur est survenue lors de la récupération des données :', error);
+  });
